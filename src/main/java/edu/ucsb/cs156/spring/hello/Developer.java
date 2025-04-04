969cfafd0985f49528963da7f61dc6df90eae457
chloeandersen-dev <chloe@andersens.com>
2025-04-04 11:53:56 -0700

    xy - updated with actual developer name

--- src/main/java/edu/ucsb/cs156/spring/hello/Developer.java
+++ src/main/java/edu/ucsb/cs156/spring/hello/Developer.java
@@ -16,11 +16,7 @@ public class Developer {
      */
 
     public static String getName() {
-        // TODO: Change this to your name
-        // You may use just the name that is used on <https://bit.ly/cs156-s25-teams>
-        // i.e. your first name, or your first and initial of last name
-
-        return "Chris G.";
+        return "Chloe A.";
     }
 
     /**
@@ -29,8 +25,7 @@ public class Developer {
      */
 
     public static String getGithubId() {
-        // TODO: Change this to your github id
-        return "cgaucho";
+        return "chloeandersen-ucsb";
     }
 
     /**
@@ -39,14 +34,13 @@ public class Developer {
      */
     
     public static Team getTeam() {
-        // TODO: Change this to your team name
-        Team team = new Team("s25-xx");
-        team.addMember("Alice");
-        team.addMember("Bob");
-        team.addMember("Chris G.");
-        team.addMember("Danny");
-        team.addMember("Eve");
-        team.addMember("Frances");
+        Team team = new Team("s25-14");
+        team.addMember("Andrew Z.");
+        team.addMember("Chloe A.");
+        team.addMember("Karen Y.");
+        team.addMember("Ryan C.");
+        team.addMember("Shruthi S.");
+        team.addMember("Siddhi M.");
         return team;
     }
 }
