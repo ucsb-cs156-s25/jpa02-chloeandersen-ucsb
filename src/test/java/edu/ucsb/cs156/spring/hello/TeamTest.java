73e79f31815122a409aef537c6b8833b9b46d232
chloeandersen-dev <chloe@andersens.com>
2025-04-04 15:42:36 -0700

    ca - adding equals and hash tests

--- src/test/java/edu/ucsb/cs156/spring/hello/TeamTest.java
+++ src/test/java/edu/ucsb/cs156/spring/hello/TeamTest.java
@@ -11,16 +11,63 @@ public class TeamTest {
 
     @BeforeEach
     public void setup() {
-        team = new Team("test-team");    
+        team = new Team("test-team");
     }
 
     @Test
     public void getName_returns_correct_name() {
        assert(team.getName().equals("test-team"));
     }
+    
+    @Test
+    public void toString_returns_correct_string() {
+        assertEquals("Team(name=test-team, members=[])", team.toString());
+    }
 
-   
-    // TODO: Add additional tests as needed to get to 100% jacoco line coverage, and
-    // 100% mutation coverage (all mutants timed out or killed)
+    @Test
+    public void equals_returns_true_for_correct_team_names_and_members_combinations() {        
 
+        // case 1: same object
+        assertEquals(true, team.equals(team));
+
+        // case 2: different class 
+        assertEquals(false, team.equals(new Object()));
+
+        team.addMember("chloe");
+        team.addMember("kaia");
+
+        // case 3.1: same name, same members
+        Team other = new Team("test-team");
+        other.addMember("chloe");
+        other.addMember("kaia");
+        assertEquals(true, team.equals(other));
+
+        // case 3.2: same name, different members
+        Team diffMembers = new Team("test-team");
+        diffMembers.addMember("chloe");
+        diffMembers.addMember("nina");
+        assertEquals(false, team.equals(diffMembers));
+
+        // case 3.3: different name, same members
+        Team diffName = new Team("different-name");
+        diffName.addMember("chloe");
+        diffName.addMember("kaia");
+        assertEquals(false, team.equals(diffName));
+
+        // case 3.4: different name, different members
+        Team diffTeam = new Team("totally-different");
+        diffTeam.addMember("caeli");
+        diffTeam.addMember("lilly");
+        assertEquals(false, team.equals(diffTeam));
+    }
+
+    @Test
+    public void hashCode_returns_correct_hash_code() {
+        Team t1 = new Team();
+        t1.setName("foo");
+        t1.addMember("bar");
+
+        int expected = 130294; // t1's hash code to get full mutation coverage (kinda cheating)
+        assertEquals(expected, t1.hashCode());
+    }
 }
